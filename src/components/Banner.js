3259e7ff57632cf94e6e1f9faf7d80f92cc14a10
esmith7196 <ericsmith.100@gmail.com>
2020-11-06 14:01:08 -0500

    add texture to banner

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -2,9 +2,6 @@ import React from "react"
 import styled from "styled-components"
 import { device } from "../global-styles"
 
-import Button from "./button"
-import Title from "./Title"
-
 const BannerWrapper = styled.section`
   padding: 119px 1.5rem;
   background: ${({ theme, fillColor }) => getBG(theme, fillColor)};
@@ -12,7 +9,7 @@ const BannerWrapper = styled.section`
   display: flex;
   flex-direction: column;
   align-items: center;
-
+  background-image: url("https://www.transparenttextures.com/patterns/cubes.png");
   @media ${device.tablet} {
     padding: 119 0;
   }
@@ -32,14 +29,6 @@ const getBG = (theme, fillColor) => {
   }
   return fillColor
 }
-
-// const Title = styled.h3`
-//   color: white;
-//   font-family: ${({ theme }) => theme.fonts.headingFont};
-//   font-size: 62px;
-//   margin-bottom: 79px;
-//   margin-top: 0;
-// `
 
 const Banner = ({ children, fillColor }) => {
   return <BannerWrapper fillColor={fillColor}>{children}</BannerWrapper>
